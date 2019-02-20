3ce7a81474452a4e0d0fe0a3c74eaede8defc7cd
Kavyan1597 <47587997+Kavyan1597@users.noreply.github.com>
2019-02-20 16:52:08 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
-//Program to add two numbers
-#include <stdio.h>
-#include <stdlib.h>
-int a=10;
-int b=20;
-int sum;
-sum=a+b;
-printf("sum=%d",sum);
+//Program to check whether system in little endian or big endian and to convert
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<inttypes.h>
+void convert()
+{
+    uint32_t le=0x12345678;
+    uint32_t be=__builtin_bswap32(le);
+    printf("Little_endian: 0x%" PRIx32"\n",le);
+    printf("Big_endian: 0x%" PRIx32"\n",be);
+    return 0;
+
+}
+int main()
+{
+    unsigned int a=1;
+    char *p=(char*)&a;
+    if (*p)
+    {
+        printf("\nLittle Endian\n");
+        convert();
+    }
+    else
+    {
+        printf("\nBig Endian\n");
+    }
+}
